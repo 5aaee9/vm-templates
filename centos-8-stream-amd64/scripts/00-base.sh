8e01f24eef3c53ecec89d15be260d40aac4f823c
Indexyz <jiduye@gmail.com>
2020-03-31 10:27:44 +0800

    feat: move to self-host runner

--- centos-8-stream-amd64/scripts/00-base.sh
+++ centos-8-stream-amd64/scripts/00-base.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -ex
 
+# QEMU Guest Agent setup
 dnf install -y qemu-guest-agent epel-release
+
+# Cloud-Init
+dnf install -y cloud-init cloud-utils-growpart
+
+# PIP Install
 dnf install -y python3-pip
+
+# Software init
+dnf install -y mtr
 
 systemctl enable qemu-guest-agent
