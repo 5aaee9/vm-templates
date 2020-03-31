7a6bcbed684f6505f7e71440d6f17a921c5ee5ce
Indexyz <jiduye@gmail.com>
2020-03-31 12:38:31 +0800

    fix(init): cloud-init acpid

--- centos-8-stream-amd64/scripts/00-base.sh
+++ centos-8-stream-amd64/scripts/00-base.sh
@@ -3,14 +3,14 @@ set -ex
 
 # QEMU Guest Agent setup
 dnf install -y qemu-guest-agent epel-release
+systemctl enable qemu-guest-agent
 
 # Cloud-Init
-dnf install -y cloud-init cloud-utils-growpart
+dnf install -y acpid cloud-init cloud-utils-growpart
+systemctl enable acpid
 
 # PIP Install
 dnf install -y python3-pip
 
 # Software init
 dnf install -y mtr
-
-systemctl enable qemu-guest-agent
