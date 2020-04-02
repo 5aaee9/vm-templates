c41833477970687ccff20a087938fdb1c587acfb
Indexyz <jiduye@gmail.com>
2020-04-02 22:29:49 +0800

    fix(docker): entrypoint error

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@ set -ex
 
 export RUNNER_ALLOW_RUNASROOT=1
 
-./config --url "${RUNNER_REPO}" \
+./config.sh --url "${RUNNER_REPO}" \
     --token "${RUNNER_TOKEN}" \
     --name "${RUNNER_NAME}" \
     --work /runner/_work
