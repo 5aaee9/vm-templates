4bebcea89644de5eeab21d42e556c30d7b6439e9
Indexyz <jiduye@gmail.com>
2020-04-04 01:22:58 +0800

    fix: centos 6 hwaddr error

--- centos-6-amd64/scripts/99-cleanup.sh
+++ centos-6-amd64/scripts/99-cleanup.sh
@@ -4,9 +4,16 @@ set -ux
 # clean caches
 yum -y clean all
 
-# Clean Cloud-Init
-cloud-init clean --logs
-service cloud-init stop
-rm -rf /var/lib/cloud/
+UDEV_RULES=(
+    '70-persistent-net.rules'
+)
+
+for rule in "${UDEV_RULES[@]}"; do
+    rm -f "/etc/udev/rules.d/${rule}"
+    ln -sf /dev/null "/etc/udev/rules.d/${rule}"
+done
+
+# Clean UP HDADDR in eth0
+sed -i '/^HWADDR=".*"$/d' /etc/sysconfig/network-scripts/ifcfg-eth0
 
 sync
