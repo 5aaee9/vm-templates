fc4c55f324d9a095b04d32603682a58125bcf5ab
Indexyz <jiduye@gmail.com>
2020-04-03 23:27:38 +0800

    fix: cloud init install

--- centos-6-amd64/scripts/00-base.sh
+++ centos-6-amd64/scripts/00-base.sh
@@ -12,14 +12,22 @@ export CLOUD_INIT_VERSION="20.1"
 yum install centos-release-scl -y
 yum install git wget rh-python36 -y
 
-PYTHON_PATH="/opt/rh/rh-python36/root/usr/bin"
-$PYTHON_PATH/pip install -r requirements.txt
-$PYTHON_PATH/python setup.py build
-$PYTHON_PATH/python setup.py install --init-system sysvinit
+
 cd /usr/local/src/
 wget https://launchpad.net/cloud-init/trunk/${CLOUD_INIT_VERSION}/+download/cloud-init-${CLOUD_INIT_VERSION}.tar.gz
 tar zxvf cloud-init-${CLOUD_INIT_VERSION}.tar.gz
 rm -f cloud-init-${CLOUD_INIT_VERSION}.tar.gz
+
+PYTHON_PATH="/opt/rh/rh-python36/root/usr/bin"
+cd cloud-init-${CLOUD_INIT_VERSION}
+
+$PYTHON_PATH/pip install -r requirements.txt
+$PYTHON_PATH/python setup.py build
+$PYTHON_PATH/python setup.py install --init-system sysvinit
+
+cd ..
+rm -rf cloud-init-${CLOUD_INIT_VERSION}
+
 
 for bin in cloud-id cloud-init cloud-init-per; do
     ln -s $PYTHON_PATH/$bin /usr/bin/$bin
