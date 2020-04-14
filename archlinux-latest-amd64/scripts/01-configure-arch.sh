83ec666af8b253e71317a26857d992cc77699ad5
Indexyz <jiduye@gmail.com>
2020-04-14 11:30:19 +0800

    remove ifnames

--- archlinux-latest-amd64/scripts/01-configure-arch.sh
+++ archlinux-latest-amd64/scripts/01-configure-arch.sh
@@ -19,7 +19,7 @@ $CHROOT systemctl enable systemd-resolved
 
 cat > /mnt/etc/systemd/network/20-wired.network <<EOF
 [Match]
-Name=eth0
+Name=en*
 
 [Network]
 DHCP=ipv4
@@ -27,7 +27,7 @@ EOF
 
 echo "* Install GRUB"
 yes | $CHROOT pacman -S grub
-sed -i 's#^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$#\1 net.ifnames=0 biosdevname=0"#' /mnt/etc/default/grub
+# sed -i 's#^\(GRUB_CMDLINE_LINUX_DEFAULT=".*\)"$#\1 net.ifnames=0 biosdevname=0"#' /mnt/etc/default/grub
 $CHROOT grub-install --target=i386-pc $DISK
 $CHROOT grub-mkconfig -o /boot/grub/grub.cfg
 
