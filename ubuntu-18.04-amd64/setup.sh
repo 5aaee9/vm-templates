042b88f583d5dfb948dbf87e22af06573f5f81bd
Indexyz <jiduye@gmail.com>
2020-04-17 03:44:54 +0800

    feat: add ubuntu build

--- ubuntu-18.04-amd64/setup.sh
+++ ubuntu-18.04-amd64/setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ex
 
-echo "Hello"
+echo "* Install QEMU guest agent"
+apt-get update -y
+apt-get install qemu-guest-agent -y
+systemctl enable qemu-guest-agent
+
+# cloud-init cloud-guest-utils
